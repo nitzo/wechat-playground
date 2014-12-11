0a27f3df5828cc44260b3ca709f7d8c617d0a7cd
Nitzan Bar <nitzanbar@gmail.com>
2014-12-11 20:32:43 +0800

    More logging

--- middleware.js
+++ middleware.js
@@ -2,6 +2,7 @@
 var wechat = require('wechat');
 
 exports.wechatMiddleware = wechat('nitzo', function (req, res, next) {
+    console.log('wechat API accessed!');
     // message is located in req.weixin
     var message = req.weixin;
 
@@ -11,6 +12,6 @@ exports.wechatMiddleware = wechat('nitzo', function (req, res, next) {
 });
 
 exports.requestLogger = function(req, res, next) {
-    console.log('Req PATH=' + req.path + '\tReq body=' + JSON.stringify(req.body));
+    console.log('Req PATH=' + req.path + '\tReq body=' + JSON.stringify(req.body) + '\tReq.query=' + JSON.stringify(req.query));
     next();
 };
