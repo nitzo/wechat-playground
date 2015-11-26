8610263a6d2d8779dd7541a8437e2317d1c9d10e
Nitzan Bar <nitzanbar@gmail.com>
2015-11-26 12:49:40 +0200

    Added auto login from wechat browser

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var router = express.Router();
+var passport = require('passport');
 
 /* GET home page. */
-router.get('/', function(req, res) {
+router.get('/', function(req, res, next) {
   var url = 'http://' + req.host;
   //req.user = {
   //  name : 'Nitzo',
@@ -14,6 +15,10 @@ router.get('/', function(req, res) {
   if (req.user){
     user = JSON.stringify(req.user);
   }
+  else if (isUsingWechatBrowser) {
+    passport.authenticate('wechat')(req, res, next);
+  }
+
 
   res.render('index', { title: 'Express', user: user, url : url, wechat_config : JSON.stringify(wechat_config) });
 });
